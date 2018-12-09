dc77a9e5f5dbb8fcc72fdbf4f819ba2d77938a5f
TicTacMenthe <ugurcan.ekren@telecom-sudparis.eu>
2018-12-09 19:45:30 +0100

    order

--- include/LidarWrapper/RPLidar.hpp
+++ include/LidarWrapper/RPLidar.hpp
@@ -18,7 +18,6 @@ class RPLidar {
 	rp_values::ComResult send_packet(rp_values::OrderByte order, const std::vector<uint8_t> &payload={});
 
 public:
-	void print_status();
 	rp_values::ComResult get_health(data_wrappers::HealthData *health_data);
 	rp_values::ComResult get_info(data_wrappers::InfoData *info_data);
 	rp_values::ComResult get_samplerate(data_wrappers::SampleRateData *sample_rate);
@@ -26,8 +25,7 @@ public:
 	rp_values::ComResult stop_motor();
 	rp_values::ComResult read_scan_data(std::vector<uint8_t> &output_data, uint8_t size, bool to_sync = false);
 	rp_values::ComResult stop_scan();
-
-
+	void print_status();
 	void print_scan();
 
 	//Interface for fusion_lidars:
